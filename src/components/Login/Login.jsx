9816f73696eb74516f4e794bb264ccdbbc0503bc
NicoSurbayrole <nico.surba@hotmail.com>
2023-06-06 15:26:42 -0300

    "creando vista del login + registro"

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
 
 const Login = () =>{
-    return <h1>Login</h1>
-}
+    return(
+    <main className="registerMani">
+        <section className="fromRegister">
+            <h4>INICIAR SESION</h4>
+            <input className="controls" type="email" name="email" id="email" placeholder="Ingresa tu email"></input>
+            <input className="controls" type="password" name="contraseña" id="contraseña" placeholder="Ingresa una contraseña"></input>
+            <input className="botons" type="submit" value="INICIAR SESION"></input>
+            <p>¿No Estas Registrado?<Link className="link" to={'/registro'}>Click Aqui</Link></p>
+        </section>
+    </main>
+    )}
 
 
 export default Login;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,20 @@
+import "./Register.css";
+import { Link } from "react-router-dom";
 
-const Register = () =>{
-    return <h1>REGISTRO</h1>
-}
-
-
-
+const Register = () => {
+  return (
+    <main className="registerMani">
+        <section className="fromRegister">
+            <h4>REGISTRARSE</h4>
+            <input className="controls" type="text" name="nombre" id="nombre" placeholder="Ingresa tu nombre"></input>
+            <input className="controls" type="text" name="apellido" id="apellido" placeholder="Ingresa tu apellido"></input>
+            <input className="controls" type="email" name="email" id="email" placeholder="Ingresa tu email"></input>
+            <input className="controls" type="password" name="contraseña" id="contraseña" placeholder="Ingresa una contraseña"></input>
+            <input className="botons" type="submit" value="REGISTRARSE"></input>
+            <p>¿Ya Tengo Cuenta?<Link className="link" to={'/login'}>Click Aqui</Link></p>
+        </section>
+    </main>
+  );
+};
 
 export default Register;
